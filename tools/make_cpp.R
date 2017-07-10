eab4cd2d0aa612b1e472cbe40dbc874f92fe03c6
Jouni Helske <jouni.helske@jyu.fi>
2017-07-10 12:09:27 +0100

    fix warning from new rstan version

--- tools/make_cpp.R
+++ tools/make_cpp.R
@@ -21,7 +21,7 @@ cat(readLines(file.path("inst", "chunks", "license.stan")),
   "#define STAN__SERVICES__COMMAND_HPP", "#include <rstan/rstaninc.hpp>",
   sapply(stan_files, FUN = function(f) {
     cppcode <- rstan::stanc_builder(f, 
-                 isystem = file.path("inst", "chunks"))$cppcode
+      isystem = file.path("inst", "chunks"))$cppcode
     cppcode <- gsub("typedef.*stan_model.*;", "", cppcode, perl = TRUE)
     return(cppcode)
   }), "#endif", file = file.path("src", "include", "models.hpp"), 
@@ -31,17 +31,17 @@ options("useFancyQuotes" = FALSE)
 
 sapply(sub(".stan", "", basename(stan_files), fixed = TRUE), function(f) {
   Rcpp::exposeClass(class = paste0("model_", f),
-                    constructors = list(c("SEXP", "SEXP")), fields = character(),
-                    methods = c("call_sampler", 
-                                "param_names", "param_names_oi", "param_fnames_oi", 
-                                "param_dims",  "param_dims_oi", "update_param_oi", "param_oi_tidx", 
-                                "grad_log_prob", "log_prob", 
-                                "unconstrain_pars", "constrain_pars", "num_pars_unconstrained", 
-                                "unconstrained_param_names", "constrained_param_names"), 
-                    file = paste0(f, "Module.cc"), header = '#include "include/models.hpp"', 
-                    module = paste0("stan_fit4", f, "_mod"), 
-                    CppClass = paste0("rstan::stan_fit<model_", f, "_namespace::model_", f,
-                                      ", boost::random::ecuyer1988> "),
-                    Rfile = FALSE)
+    constructors = list(c("SEXP", "SEXP", "SEXP")), fields = character(),
+    methods = c("call_sampler", 
+      "param_names", "param_names_oi", "param_fnames_oi", 
+      "param_dims",  "param_dims_oi", "update_param_oi", "param_oi_tidx", 
+      "grad_log_prob", "log_prob", 
+      "unconstrain_pars", "constrain_pars", "num_pars_unconstrained", 
+      "unconstrained_param_names", "constrained_param_names"), 
+    file = paste0(f, "Module.cc"), header = '#include "include/models.hpp"', 
+    module = paste0("stan_fit4", f, "_mod"), 
+    CppClass = paste0("rstan::stan_fit<model_", f, "_namespace::model_", f,
+      ", boost::random::ecuyer1988> "),
+    Rfile = FALSE)
   return(invisible(NULL))
 })
