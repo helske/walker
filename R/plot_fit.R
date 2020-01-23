2e86f49765656ce97201e04183f9f7573dbcd7f5
helske <jouni.helske@iki.fi>
2020-01-23 10:27:06 +0200

    strip NAs from plot_fit

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -12,8 +12,10 @@ plot_fit <- function(object, level = 0.05, alpha = 0.33, ...){
     y_fit <- y_fit[sample(1:nrow(y_fit), size = nrow(y_fit), replace = TRUE, 
       prob = extract(object$stanfit, pars = "weights", permuted = TRUE)$weights),  , drop = FALSE]
   }
-  ppc_ribbon(y = as.numeric(object$y), 
-    yrep = y_fit,
-    x = as.numeric(time(object$y)), ...) + theme(legend.position = "none") + 
+  noNA <- which(!is.na(object$y))
+  ppc_ribbon(y = as.numeric(object$y[noNA]), 
+    yrep = y_fit[,noNA],
+    x = as.numeric(time(object$y))[noNA], ...) + 
+    theme(legend.position = "none") + 
     scale_x_continuous(name = "time")
 }
