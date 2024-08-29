0fec72bc5d44852b306cc829d9d660294b716366
Jouni Helske <jouni.helske@utu.fi>
2024-08-29 13:09:16 +0300

    to CRAN

--- R/pp_check.R
+++ R/pp_check.R
@@ -6,7 +6,7 @@
 #' For other types of posterior predictive checks for example with `bayesplot`, 
 #' you can extract the variable `yrep` from the output, see examples.
 #' 
-#' @importFrom bayesplot pp_check
+#' @importFrom bayesplot pp_check ppc_ribbon
 #' @param object An output from [walker()].
 #' @param ... Further parameters to [bayesplot::ppc_ribbon()].
 #' @export
@@ -14,10 +14,10 @@
 #' \dontrun{
 #' # Extracting the yrep variable for general use:
 #' # extract yrep
-#' y_rep <- rstan::extract(object$stanfit, pars = "y_rep", permuted = TRUE)$y_rep
+#' y_rep <- extract(object$stanfit, pars = "y_rep", permuted = TRUE)$y_rep
 #' 
 #' # For non-gaussian model:
-#' weights <- rstan::extract(object$stanfit, 
+#' weights <- extract(object$stanfit, 
 #' pars = "weights", permuted = TRUE)$weights
 #' y_rep <- y_rep[sample(1:nrow(y_rep), 
 #'   size = nrow(y_rep), replace = TRUE, prob = weights), , drop = FALSE]
